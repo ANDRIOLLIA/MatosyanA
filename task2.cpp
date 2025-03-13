7628cba9d74cd274b6070aec2496725c2a3ec0ca
ANDRIOLLIA <andreymatosian94@gmail.com>
2025-03-13 10:25:36 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -11,7 +11,7 @@ public:
 		name = "Земля";
 	}
 
-	void add(int density, int weight_10_24, int diameter, std::string name) {
+	Planet(int density, double weight_10_24, int diameter, std::string name) {
 		this->density = density;
 		this->weight_10_24 = weight_10_24;
 		this->diameter = diameter;
@@ -19,7 +19,7 @@ public:
 	}
 
 	void info() {
-		std::cout << "Планета: " << name << ", " << density << "-я планета от солнца, диаметр: "
+		std::cout << "Название: " << name << ", " << density << ", диаметр: "
 			<< diameter << ", вес: " << weight_10_24;
 	}
 };
@@ -28,14 +28,13 @@ int main() {
 	system("chcp 1251"); system("cls");
 	std::vector <Planet> planets;
 
-	planets.at(0).add(5427, 0.33, 4873, "Меркурий");
-	planets.at(1).add(5243, 4.87, 12104, "Венера");
-	planets.at(2).add(5515, 5.97, 12756, "Земля");
-	planets.at(3).add(3340, 0.073, 3475, "Луна");
-	planets.at(4).add(3933, 0.642, 6794, "Марс");
-	planets.at(5).add(1326, 1899, 142984, "Юпитер");
-	planets.at(6).add(687, 568, 120536, "Сатурн");
-
+	planets.push_back(Planet(5427, 0.33, 4873, "Меркурий"));
+	planets.push_back(Planet(5243, 4.87, 12104, "Венера"));
+	planets.push_back(Planet(5515, 5.97, 12756, "Земля"));
+	planets.push_back(Planet(3340, 0.073, 3475, "Луна"));
+	planets.push_back(Planet(3933, 0.642, 6794, "Марс"));
+	planets.push_back(Planet(1326, 1899, 142984, "Юпитер"));
+	planets.push_back(Planet(687, 568, 120536, "Сатурн"));
 
 	for (int i = 0; i < planets.size(); i++) {
 		planets.at(i).info();
