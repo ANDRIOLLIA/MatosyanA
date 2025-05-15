7127b0d1ed257e2dd5e18614f0948ed82119b645
ANDRIOLLIA <andreymatosian94@gmail.com>
2025-05-15 10:27:01 +0300

    asd
    asd

--- task_mech_luk_vipit.cpp
+++ task_mech_luk_vipit.cpp
@@ -6,22 +6,28 @@ public:
 };
 
 class Sword : public Use{
-	int weight, damage;
+	int weight, damage, random_status;
 	double length;
 public:
 	Sword() {
 		weight = 15;
 		length = 1.65;
-		damage = rand() % 5 + 15;
+		damage = (rand() % 5 + 15) * weight * 0.3;
+		random_status = rand() % 2;
 	}
 
 	void use() override {
-		std::cout << "\nsword-> Вы нанесли " << damage << " урона по мишени";
+		if (random_status == 0)
+			std::cout << "SWORD-> Вы нанесли " << damage << " ед. урона";
+		else if (random_status == 1) {
+			std::cout << "SWORD-> У вас сломался меч";
+			damage = 0;
+		}
 	}
 };
 
 class Bow : public Use {
-	int weight, bowstring_tension, distance, damage;
+	int weight, bowstring_tension, distance, damage, random_status;
 	double length;
 public:
 	Bow() {
@@ -30,10 +36,18 @@ public:
 		bowstring_tension = rand() % 5 + 1;
 		distance = bowstring_tension * 10;
 		damage = 3 * bowstring_tension;
+		random_status = rand() % 3;
 	}
 
 	void use() override {
-		std::cout << "\nbow-> Вы выстрелили на расстояние в " << distance << " метров и нанесли " << damage << " ед. урона";
+		if (random_status == 0)
+			std::cout << "BOW-> Вы выстрелили на расстояние в " << distance << " метров и нанесли " << damage << " ед. урона";
+		else if (random_status == 1) {
+			std::cout << "BOW-> У вас порвалась тетева, лох";
+			damage = 0;
+		}
+		else if (random_status == 2)
+			std::cout << "BOW-> Стрела улетела в стратосферу, подожгла цыганское поселение и нанесла " << damage * 100 << " ед. урона по области";
 	}
 };
 
@@ -46,7 +60,7 @@ public:
 		is_heal = rand() % 2;
 	}
 	void use() override {
-		std::cout << "\npotion-> Вы выпили " << volume << " мл. зелья и ";
+		std::cout << "POTION-> Вы выпили " << volume << " мл. зелья и ";
 		if (is_heal)std::cout << "исцелили 10 hp (не ноутбуков)";
 		else std::cout << "отравились";
 	}
@@ -68,13 +82,13 @@ public:
 };
 
 int main() {
+	srand(time(NULL));
 	system("chcp 1251"); system("cls");
 	Player andrey;
 	std::shared_ptr<Sword> sword = std::make_shared<Sword>();
 	std::shared_ptr<Bow> bow = std::make_shared<Bow>();
 	std::shared_ptr<Potion> potion = std::make_shared<Potion>();
-	srand(time(NULL));
-	andrey.use(sword);
-	andrey.use(bow);
-	andrey.use(potion);
+	andrey.use(sword); std::cout << "\n\n";
+	andrey.use(bow); std::cout << "\n\n";
+	andrey.use(potion);std::cout << "\n";
 }
