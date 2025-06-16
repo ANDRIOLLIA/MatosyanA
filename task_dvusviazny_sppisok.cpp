81f887bee258ca9c7feea701de232f4da793c2f8
ANDRIOLLIA <andreymatosian94@gmail.com>
2025-06-16 12:54:56 +0300

    asd
    asd

--- task_dvusviazny_sppisok.cpp
+++ task_dvusviazny_sppisok.cpp
@@ -1,92 +1,53 @@
 #include <iostream>
 
-class Node {
+class Queue {
+	class Node {
+	public:
+		std::shared_ptr<int> data;
+		std::shared_ptr<Node> next;
+		Node(int val) : data{ std::make_shared<int>(val)}, next{ nullptr } {}
+	};
+	std::shared_ptr<Node> first, last;
 public:
-    std::shared_ptr<int> data;
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+	Queue() {
+		first = last = nullptr;
+	}
 
-    Node(std::shared_ptr<int> val) : data(val), next(nullptr), prev(nullptr) {}
-    Node() : data(0), next(nullptr), prev(nullptr) {}
-    ~Node() {
-        delete &data;
-        delete &next;
-        delete &prev;
-    }
-};
+	Queue& push(int val) {
+		if (!first) { first = last = std::make_shared<Node>(val); return *this; }
+		last->next = std::make_shared<Node>(val);
+		last = last->next;
+		return *this;
+	}
 
-class List {
-    std::shared_ptr<Node> first;
-    std::shared_ptr<Node> last;
-    int size;
-
-public:
-    List() : first(nullptr), last(nullptr), size(0) {}
-
-    ~List() {
-        clear();
-    }
-
-    void pushBack(int val) {
-        std::shared_ptr<Node> node = std::make_shared<Node>();
-        if (first == nullptr) {
-            first = last = node;
-        }
-        else {
-            last->next = node;
-            node->prev = last;
-            last = node;
-        }
-        ++size;
-    }
-
-    void popBack() {
-        if (last == nullptr) return;
-        std::shared_ptr<Node> to_delete = last;
-        last = last->prev;
-        if (last) last->next = nullptr;
-        else first = nullptr;
-        delete &to_delete;
-        --size;
-    }
-
-    int& operator[](int index) {
-        if (index < 0 || index >= size) {
-            return *first->data;
-        }
-        std::shared_ptr<Node> current = first;
-        for (int i = 0; i < index; ++i)
-            current = current->next;
-        return *current->data;
-    }
-
-    void clear() {
-        std::shared_ptr<Node> current = first;
-        while (current != nullptr) {
-            std::shared_ptr<Node> nextNode = current->next;
-            delete &current;
-            current = nextNode;
-        }
-        first = nullptr;
-        last = nullptr;
-        size = 0;
-    }
-
-    friend std::ostream& operator<<(std::ostream& out, List& list) {
-        std::shared_ptr<Node> current = list.first;
-        while (current != nullptr) {
-            out << current->data << ' ';
-            current = current->next;
-        }
-        return out;
-    }
+	int pop() {
+		int data = *first->data;
+		first = first->next;
+		return data;
+	}
+	bool isEmpty() { return first == nullptr; }
+	
+	Queue& clear() {
+		while (first) { pop(); }
+		last = first = nullptr;
+		return *this;
+	}
+	
+	~Queue() {
+		clear();
+	}
 };
 
 int main() {
-    List l;
-
-    for (int i = 0; i < 1000000; ++i) {
-        l.pushBack(i);
-    }
-    return 0;
+	Queue q1; std::cout << (q1.isEmpty() ? "\nempty" : "\nnot empty");
+	for (int i = 0; i < 100; i++)q1.push(i);
+	for (int i = 0; i < 5; i++) {
+		std::cout << q1.pop() << " ";
+	}
+	std::cout << (q1.isEmpty() ? "\nempty" : "\nnot empty");
+	for (int i = 0; i < 10; i++) {
+		q1.push(i);
+	}
+	q1.clear();
+	std::cout << (q1.isEmpty() ? "\nempty" : "\nnot empty");
 }
