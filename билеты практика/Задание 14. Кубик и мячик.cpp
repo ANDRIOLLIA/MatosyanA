463fde3c6620225197f5b4882d9bd5e85823c0f0
ANDRIOLLIA <andreymatosian94@gmail.com>
2025-06-17 14:21:24 +0300

    asd
    asd

--- билеты практика/Задание 14. Кубик и мячик.cpp
+++ билеты практика/Задание 14. Кубик и мячик.cpp
@@ -2,15 +2,35 @@
 
 class Body {
 	int x, y, z;
-	float jumpable;
+	float jumpable = rand() % 1 * 0.1;
 public:
 	virtual int dropIt(int force) = 0;
+	float getJumpable() const {
+		return jumpable;
+	}
+	int getY() const {
+		return y;
+	}
 };
 
-class Dice {
+class Dice : public Body{
 	int num;
+public:
+	int dropIt(int force) override {
+		int total = force * getJumpable() * getY();
+		for (int i = 0; i < total; i++) {
+			num = i;
+			if (num > 6 || num == 0) num = 1;
+		}
+		return num;
+	}
 };
 
-class Ball {
+class Ball : public Body {
 
 };
+
+int main() {
+	Dice dice;
+	std::cout << dice.dropIt(10);
+}
